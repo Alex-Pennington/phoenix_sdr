c52f147652c4edf1d0af2954806230f4278e3bb6
Alex-Pennington <alex.pennington@organicengineer.com>
2025-12-14 00:26:40 -0500

    v0.2.5 build 10

--- include/version.h
+++ include/version.h
@@ -14,10 +14,10 @@
 #define PHOENIX_VERSION_MAJOR   0
 #define PHOENIX_VERSION_MINOR   2
 #define PHOENIX_VERSION_PATCH   5
-#define PHOENIX_VERSION_BUILD   9
+#define PHOENIX_VERSION_BUILD   10
 #define PHOENIX_VERSION_STRING  "0.2.5"
-#define PHOENIX_VERSION_FULL    "0.2.5+9.d60aa68"
-#define PHOENIX_GIT_COMMIT      "d60aa68"
+#define PHOENIX_VERSION_FULL    "0.2.5+10.f2ed173"
+#define PHOENIX_GIT_COMMIT      "f2ed173"
 #define PHOENIX_GIT_DIRTY       false
 
 /* Build timestamp - set by compiler */

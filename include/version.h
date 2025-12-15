56f2b83f0107c532b687cb60441ccfb955b0021d
Alex-Pennington <alex.pennington@organicengineer.com>
2025-12-14 21:03:15 -0500

    Update version information to 0.3.0+57.70c2d2a and increment build number

--- include/version.h
+++ include/version.h
@@ -14,10 +14,10 @@
 #define PHOENIX_VERSION_MAJOR   0
 #define PHOENIX_VERSION_MINOR   3
 #define PHOENIX_VERSION_PATCH   0
-#define PHOENIX_VERSION_BUILD   56
+#define PHOENIX_VERSION_BUILD   57
 #define PHOENIX_VERSION_STRING  "0.3.0"
-#define PHOENIX_VERSION_FULL    "0.3.0+56.b425fe9-dirty"
-#define PHOENIX_GIT_COMMIT      "b425fe9"
+#define PHOENIX_VERSION_FULL    "0.3.0+57.b29b363-dirty"
+#define PHOENIX_GIT_COMMIT      "b29b363"
 #define PHOENIX_GIT_DIRTY       true
 
 /* Build timestamp - set by compiler */

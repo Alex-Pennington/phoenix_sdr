021b20dde84c2f1dc02e5efc16fe8c2bf09745b5
Alex-Pennington <alex.pennington@organicengineer.com>
2025-12-16 07:33:08 -0500

    Update version to 0.3.2+110.c1edc4e and increment build number to 110

--- include/version.h
+++ include/version.h
@@ -14,10 +14,10 @@
 #define PHOENIX_VERSION_MAJOR   0
 #define PHOENIX_VERSION_MINOR   3
 #define PHOENIX_VERSION_PATCH   2
-#define PHOENIX_VERSION_BUILD   109
+#define PHOENIX_VERSION_BUILD   110
 #define PHOENIX_VERSION_STRING  "0.3.2"
-#define PHOENIX_VERSION_FULL    "0.3.2+109.unknown"
-#define PHOENIX_GIT_COMMIT      "unknown"
+#define PHOENIX_VERSION_FULL    "0.3.2+110.ba227a3"
+#define PHOENIX_GIT_COMMIT      "ba227a3"
 #define PHOENIX_GIT_DIRTY       false
 
 /* Build timestamp - set by compiler */

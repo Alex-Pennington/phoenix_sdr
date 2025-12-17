af99aea5493e8b36fa625f83a59efe5659aa7ddd
Alex-Pennington <alex.pennington@organicengineer.com>
2025-12-16 22:34:59 -0500

    Update version to 0.3.2+152, increment build number and reset dirty flag

--- include/version.h
+++ include/version.h
@@ -14,11 +14,11 @@
 #define PHOENIX_VERSION_MAJOR   0
 #define PHOENIX_VERSION_MINOR   3
 #define PHOENIX_VERSION_PATCH   2
-#define PHOENIX_VERSION_BUILD   151
+#define PHOENIX_VERSION_BUILD   152
 #define PHOENIX_VERSION_STRING  "0.3.2"
-#define PHOENIX_VERSION_FULL    "0.3.2+151.9e4a118-dirty"
-#define PHOENIX_GIT_COMMIT      "9e4a118"
-#define PHOENIX_GIT_DIRTY       true
+#define PHOENIX_VERSION_FULL    "0.3.2+152.1be9ccd"
+#define PHOENIX_GIT_COMMIT      "1be9ccd"
+#define PHOENIX_GIT_DIRTY       false
 
 /* Build timestamp - set by compiler */
 #define PHOENIX_BUILD_DATE      __DATE__
